9d91c0daee274d812990115a5bc2fffe2b9790b2
gautam-khaire <gautam.khaire@stride.ai>
2024-04-03 12:20:11 +0530

    Object Detection AI model loaded from tensorflow

--- components/object-detection.js
+++ components/object-detection.js
@@ -1,10 +1,17 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
+import { load as cocoSSDLoad } from "@tensorflow-models/coco-ssd";
+import * as tf from "@tensorflow/tfjs";
+import { renderPredictions } from "@/utils/render-predictions";
+let detectInterval;
 
 const ObjectDetection = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
   const showmyVideo = () => {
     if (
       webcamRef.current !== null &&
@@ -18,21 +25,65 @@ const ObjectDetection = () => {
     }
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     showmyVideo();
-  },[]);
+    runCoco();
+  }, []);
+
+  const runCoco = async () => {
+    setIsLoading(true);
+    const net = await cocoSSDLoad();
+    setIsLoading(false);
+
+    detectInterval = setInterval(() => {
+      runObjectDetection(net);
+    }, 10);
+  };
+
+  const runObjectDetection = async (net) => {
+    if (
+      canvasRef.current &&
+      webcamRef.current !== null &&
+      webcamRef.current.video?.readyState === 4
+    ) {
+      canvasRef.current.width = webcamRef.current.video.videoWidth;
+      canvasRef.current.height = webcamRef.current.video.videoHeight;
+
+      // find detected objects
+      const detectedObjects = await net.detect(
+        webcamRef.current.video,
+        undefined,
+        0.8
+      );
+
+    //   console.log(detectedObjects);
+
+        const context = canvasRef.current.getContext("2d");
+        renderPredictions(detectedObjects,context);
+    }
+  };
+
   
   return (
     <div className="mt-8">
-      <div className="relative flex justify-center items-center gradient p-1.5 rounded-md">
-        {/* webcam */}
-        <Webcam
-          ref={webcamRef}
-          className="rounded-md w-full lg:h-[720px]"
-          muted
-        />
-        {/* canvas */}
-      </div>
+      {isLoading ? (
+        <div className="gradient-text">Loading AI Model</div>
+      ) : (
+        <div className="relative flex justify-center items-center gradient p-1.5 rounded-md">
+          {/* webcam */}
+          <Webcam
+            ref={webcamRef}
+            className="rounded-md w-full lg:h-[720px]"
+            muted
+          />
+
+          {/* canvas */}
+          <canvas
+            ref={canvasRef}
+            className="absolute top-0 left-0 z-99999 w-full lg:h-[720px]"
+          />
+        </div>
+      )}
     </div>
   );
 };
