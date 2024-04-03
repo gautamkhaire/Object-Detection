0c33eeca9bb24b5f9ba441a5e07327307a86ca07
gautam-khaire <gautam.khaire@stride.ai>
2024-04-03 11:20:52 +0530

    Next.js and Tailwindcss Configured

---
+++ components/object-detection.js
@@ -0,0 +1,12 @@
+const ObjectDetection = () => {
+  return (
+    <div className="mt-8">
+      <div className="relative flex justify-center items-center gradient p-1.5 rounded-md">
+        {/* webcam */}
+        {/* canvas */}
+      </div>
+    </div>
+  );
+};
+
+export default ObjectDetection;
